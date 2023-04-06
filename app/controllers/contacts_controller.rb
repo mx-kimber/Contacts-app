2a8bae864d5d2417c7aed3eae42dda9e203ee901
Kimber Ogden <mx.kimber@gmail.com>
2023-04-06 01:10:39 -0500

    add create action / rename products to contacts - oops

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,23 @@
 class ContactsController < ApplicationController
 
   def index
-    render json: {message: "this is working"}
+    @contacts = Contact.all
+    render :index
   end
 
   def show
-    @product = Product.find_by(id: params[:id])
+    @contact = contact.find_by(id: params[:id])
     render :show
   end
 
+  def create
+    @contact = contact.new(
+    contact: first_name = params[:first_name],
+    contact: last_name = params[:first_name],
+    contact: email = params[:email],
+    contact: phone_number = params[:phone_number] # remember comma when adding
+
+    )
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/contacts" => "contacts#index"
   get "/contasts/:id" => "contacts#show"
+  post "/contacts" => "contacts#create"
 
 end
