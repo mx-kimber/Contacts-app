c872c1247f91e5bbec2d40f7e92a85556190fa08
Kimber Ogden <mx.kimber@gmail.com>
2023-04-06 01:00:37 -0500

    add show action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,4 +4,9 @@ class ContactsController < ApplicationController
     render json: {message: "this is working"}
   end
 
+  def show
+    @product = Product.find_by(id: params[:id])
+    render :show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/contacts" => "contacts#index"
+  get "/contasts/:id" => "contacts#show"
 
 end
